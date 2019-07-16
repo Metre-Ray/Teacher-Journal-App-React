f5750172ac152cce9ed8e389521783b451b94c9c
Diana Loika <Diana_Loika@epam.com>
2019-07-16 15:25:20 +0300

    feat: add markup for export page, statistics page

--- src/components/main/export/ExportPage.jsx
+++ src/components/main/export/ExportPage.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
+import './ExportPage.scss';
+
 export default class ExportPage extends Component {
   render() {
     return (
-      <div>
-        Export!
+      <div className="export-page-container">
+        <button
+          className="export-page-container__pdf"
+          aria-label="Export PDF">
+          <i className="far fa-file-pdf"></i>
+        </button>
+        <button
+          className="export-page-container__excel"
+          aria-label="Export Excel">
+          <i className="far fa-file-excel"></i>
+        </button>
       </div>
     )
   }
--- src/components/main/statistics/StatisticsPage.jsx
+++ src/components/main/statistics/StatisticsPage.jsx
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
 
+import './StatisticsPage.scss';
+
 export default class StatisticsPage extends Component {
   render() {
+    // const { students, subjects } = this.props;
+    const students = [];
+    const subjects = [];
+
     return (
-      <div>
-        Statistics!
+      <div class="statistics-page-container">
+
+        <div class="panel-container">
+          <div class="panel-container__buttons">
+            <button>
+              Students
+            </button>
+            <button>
+              Subjects
+            </button>
+          </div>
+          <div class="panel-container__list">
+            <ol>
+              {
+                students.map((student) => {
+                  <li tabindex="0">
+                    {student.name + ' ' + student.lastName}
+                  </li>
+                })
+              }
+            </ol>
+            <ol>
+              {
+                subjects.map((subject) => {
+                  <li tabindex="0">
+                    {subject.name}
+                  </li>
+                })
+              }
+            </ol>
+          </div>
+        </div>
+
+        <div class="statistics-container">
+          <div class="statistics-container__data">
+            <div>
+              <span>Student</span>
+            </div>
+            <div>
+              <span>Average marks</span>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
